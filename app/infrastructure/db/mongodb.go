17b965669abea2be017830101e269065ee77fde9
martinezdelprenois <martintumwine@gmail.com>
2022-02-22 14:00:02 +0300

    created the infrastructure

---
+++ app/infrastructure/db/mongodb.go
@@ -0,0 +1,61 @@
+package db
+
+import (
+	"context"
+	"log"
+
+	"../../domain/dto"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+type DBHandler struct {
+	MongoClient mongo.Client
+	database    *mongo.Database
+}
+
+func NewDBHandler(connectString string, dbName string) (DBHandler, error) {
+	dbHandler := DBHandler{}
+	clientOptions := options.Client().ApplyURI(connectString)
+	client, err := mongo.Connect(context.TODO(), clientOptions)
+	if err != nil {
+		log.Fatal(err)
+		return dbHandler, err
+	}
+	err = client.Ping(context.TODO(), nil)
+	if err != nil {
+		log.Fatal(err)
+		return dbHandler, err
+	}
+	dbHandler.MongoClient = *client
+	dbHandler.database = client.Database(dbName)
+	return dbHandler, nil
+}
+
+func (dbHandler DBHandler) FindAllUsers() ([]*dto.User, error) {
+	var results []*dto.User
+	collection := dbHandler.database.Collection("users")
+	cur, err := collection.Find(context.TODO(), bson.D{{}})
+	if err != nil {
+		return nil, err
+	}
+	for cur.Next(context.TODO()) {
+		var elem dto.User
+		err2 := cur.Decode(&elem)
+		if err2 != nil {
+			log.Fatal(err2)
+		}
+		results = append(results, &elem)
+	}
+	return results, nil
+}
+
+func (dbHandler DBHandler) SaveUser(user dto.User) error {
+	collection := dbHandler.database.Collection("users")
+	_, err := collection.InsertOne(context.TODO(), user)
+	if err != nil {
+		return err
+	}
+	return nil
+}
